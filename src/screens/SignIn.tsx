02a05430505c4d8f67c83135cb454c31e108ca96
rickydomingos <ricardodomingos97@gmail.com>
2022-07-21 16:29:22 -0300

    class 2 done

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -9,14 +9,21 @@ import { Input } from "../components/Input";
 import { Button } from "../components/Button";
 
 export function SignIn() {
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
 
   const { colors } = useTheme();
+
+  function handleSignIn() {
+    console.log(name, password)
+  }
+
   return (
     <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
       <Logo />
 
       <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
-        Acesse a sua conta
+        Acesse a sua conta {name}
       </Heading>
 
       
@@ -24,16 +31,18 @@ export function SignIn() {
       mb={4}
       placeholder="E-mail" 
       InputLeftElement={<Icon as={<Envelope color={colors.gray[300]} />} ml={4}/>}
+      onChangeText={setName}
       />
       <Input 
       mb={8}
       placeholder="Senha" 
       InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4}/>}
       secureTextEntry
+      onChangeText={setPassword}
       />
 
     
-    <Button title="Entrar" w="full"/>
+    <Button title="Entrar" w="full" onPress={handleSignIn}/>
     </VStack>
   );
 }
