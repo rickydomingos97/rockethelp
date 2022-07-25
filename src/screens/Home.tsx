eb675b94b44a397f67bcca26db5b0269741b2409
rickydomingos <ricardodomingos97@gmail.com>
2022-07-25 13:20:47 -0300

    flatlist render

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -6,23 +6,22 @@ import {
   useTheme,
   Text,
   Heading,
-  FlatList
+  FlatList,
+  Center,
 } from "native-base";
-import { SignOut } from "phosphor-react-native";
+import { SignOut, ChatTeardropText } from "phosphor-react-native";
 
 import Logo from "../assets/logo_secondary.svg";
 
 import { Filter } from "../components/Filter";
+import { Button } from "../components/Button";
 import { Order, OrderProps } from "../components/Order";
 
 export function Home() {
   const [statusSelected, setStatusSelected] = useState<'open' | 'closed'>('open')
-  const [orders, setOrders] = useState<OrderProps[]>([{
-    id: '123',
-    patrimony: '123456',
-    when: '24/07/2022 at 14:00',
-    status: 'open'
-  }])
+  const [orders, setOrders] = useState<OrderProps[]>([
+
+])
   const { colors } = useTheme();
 
   return (
@@ -77,8 +76,20 @@ export function Home() {
         <FlatList 
           data={orders}
           keyExtractor={item => item.id}
-          renderItem={({item}) => <Order data={item} />}
+          renderItem={({item}) => <Order data={item}/>}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ paddingBottom: 100 }}
+          ListEmptyComponent={()=> (
+            <Center>
+              <ChatTeardropText color={colors.gray[300]} size={40}/>
+              <Text color="gray.300" fontSize="xl" mt={6} textAlign="center">
+                You don't have any {'\n'}
+                requests {statusSelected === 'open' ? 'opened' : 'finished'}
+              </Text>
+            </Center>
+          )}
         />
+        <Button title="New Request" />
       </VStack>
     </VStack>
   );
