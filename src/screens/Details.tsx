1608428ab6eec1f4a629b10cbeae9c9647b6b02b
rickydomingos <ricardodomingos97@gmail.com>
2022-07-26 07:51:45 -0300

    app.routes created

---
+++ src/screens/Details.tsx
@@ -0,0 +1,12 @@
+import { VStack } from 'native-base';
+
+import { Header } from '../components/Header';
+
+export function Details() {
+  return (
+    <VStack flex={1} bg="gray.700" >
+      <Header title="Request"/>
+
+    </VStack>
+  );
+}
