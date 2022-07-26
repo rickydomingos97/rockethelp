480052c709bf92becb312f845c5c2f1255ae5ceb
rickydomingos <ricardodomingos97@gmail.com>
2022-07-26 07:08:22 -0300

    register.tsx ok

--- src/screens/Register.tsx
+++ src/screens/Register.tsx
@@ -1,9 +1,30 @@
 import { VStack } from 'native-base';
 
+import { Header } from '../components/Header'
+import { Input } from '../components/Input'
+import { Button } from '../components/Button'
+
 export function Register() {
   return (
-    <VStack>
+    <VStack flex={1} p={6} bg="gray.600">
+      <Header title="New Request"/>
+      <Input 
+      placeholder="Patrimony's Name"
+      mt={4}
+      />
 
+      <Input 
+        placeholder="Problem description"
+        flex={1}
+        textAlignVertical="top"
+        multiline
+        mt={5}
+      />
+
+      <Button 
+      title="Register"
+      mt={5}
+      />
     </VStack>
   );
 }
